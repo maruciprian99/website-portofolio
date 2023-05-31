5c7a1ec6c391dc7e3a9b969f7d9e0777d48040c7
maruciprian99 <maruciprian99@gmail.com>
2023-05-31 10:49:42 +0300

    fixing the text ident

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -3,13 +3,13 @@ import { bannerImg } from '../../assets/index';
 
 const RightBanner = () => {
   return (
-    <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
+    <div className="w-full lgl:w-1/2 flex flex-col items-center relative">
       <div className="w-[300px] h-[300px] lgl:w-[400px] lgl:h-[300px] z-10">
         <div className="flex items-center justify-center h-full">
           <img className="max-h-full" src={bannerImg} alt="bannerImg" />
         </div>
       </div>
-      <div className="absolute bottom-0 w-full flex justify-center">
+      <div className="w-[300px] lgl:w-[400px] text-center mt-4"> {/* Added width and text-center class */}
         <h2 className="text-red-800 text-3xl font-bold animate-pulse">
           <span className="animate-pulse-off">Website in development...</span>
         </h2>
